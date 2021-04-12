5aac94d0c47a866d42b22e9ce01be3d60b1576b5
mikana0918 <shogo.kusuhara@gmail.com>
2021-04-12 20:02:30 +0900

    feat: データベースとの疎通およびintelij内蔵HTTPクライアント経由の実行

--- src/main/kotlin/demo/DemoApplication.kt
+++ src/main/kotlin/demo/DemoApplication.kt
@@ -3,8 +3,9 @@ package demo
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.data.annotation.Id
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.data.relational.core.mapping.Table
+import org.springframework.data.jdbc.repository.query.Query
+import org.springframework.data.repository.CrudRepository
 
 @SpringBootApplication
 class DemoApplication
@@ -13,14 +14,11 @@ fun main(args: Array<String>) {
 	runApplication<DemoApplication>(*args)
 }
 
-@RestController
-class MessageResource {
-	@GetMapping
-	fun index(): List<Message> = listOf(
-			Message("1", "Hello!"),
-			Message("2", "Bonjour!"),
-			Message("3", "Privet!"),
-	)
+@Table("MESSAGES")
+data class Message(@Id val id: String?, val text: String)
+
+interface MessageRepository: CrudRepository<Message, String> {
+	@Query("select * from messages")
+	fun findMessages(): List<Message>
 }
 
-data class Message(val id: String?, val text: String)
