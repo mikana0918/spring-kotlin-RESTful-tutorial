5db21581f678540b5c78c5a9220f4d136e49c301
mikana0918 <shogo.kusuhara@gmail.com>
2021-04-12 19:07:29 +0900

    feat: ビルド設定を追加し、APIエンドポイントを作成

--- src/main/kotlin/demo/DemoApplication.kt
+++ src/main/kotlin/demo/DemoApplication.kt
@@ -2,6 +2,9 @@ package demo
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.annotation.Id
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
 class DemoApplication
@@ -9,3 +12,15 @@ class DemoApplication
 fun main(args: Array<String>) {
 	runApplication<DemoApplication>(*args)
 }
+
+@RestController
+class MessageResource {
+	@GetMapping
+	fun index(): List<Message> = listOf(
+			Message("1", "Hello!"),
+			Message("2", "Bonjour!"),
+			Message("3", "Privet!"),
+	)
+}
+
+data class Message(val id: String?, val text: String)
